002e9b95a5206e001d5ebd9789fac9991f943164
Artem Kovalenko <artem.kovalenko190203@gmail.com>
2023-08-14 20:11:38 +0300

    Solution 6

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -1,13 +1,21 @@
 package ru.yandex.tasks;
 
+import java.util.ArrayDeque;
+
 public class Task6BraceBalance {
     public static boolean checkBalance(String sequence) {
         /*
          * sequence - последовательность скобок []{}() длины до 10^5
          * Выход: true/false, является ли строка ПСП
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        var stack = new ArrayDeque<Character>();
+        for (int i = 0; i < sequence.length(); i++) {
+            char cur = sequence.charAt(i);
+            if (cur == '(' || cur == '[' || cur == '{') stack.add(cur);
+            else if (cur == '(' && stack.pop() != ')' || cur == '[' && stack.pop() != ']' || cur == '{' && stack.pop() != '}') return false;
+            else stack.pollFirst();
+        }
+        return stack.isEmpty();
     }
 
     public static void selfCheck() {
