69704679b709246409b6e2ec6d023dbe6fa7b9ff
Artem Kovalenko <artem.kovalenko190203@gmail.com>
2023-08-14 20:56:59 +0300

    Solution 11

--- src/main/java/ru/yandex/tasks/Task11BiggestSubarraySum.java
+++ src/main/java/ru/yandex/tasks/Task11BiggestSubarraySum.java
@@ -8,8 +8,23 @@ public class Task11BiggestSubarraySum {
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: Subarray
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        var result = new Subarray();
+        var left = 0;
+        var max = numbers[0];
+        var curSum = numbers[0];
+        for (int right = 1; right < numbers.length; right++) {
+            curSum += numbers[right];
+            if (numbers[right] >= max) {
+                max = numbers[right];
+                left = right;
+            }
+            if (max <= curSum) {
+                max = curSum;
+                result.left = left;
+                result.right = right + 1;
+            }
+        }
+        return result;
     }
 
     public static void selfCheck() {
