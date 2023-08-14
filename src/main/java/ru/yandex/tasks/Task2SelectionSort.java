0e0aa80d543521d5bd8f5105b6246ea0b2869885
Artem Kovalenko <artem.kovalenko190203@gmail.com>
2023-08-14 18:55:50 +0300

    Solution 2

--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -9,8 +9,18 @@ public class Task2SelectionSort {
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
          * Выход: отсортированный (сортировкой выбором!) numbers
          */
-        // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < numbers.size(); j++) {
+                if (numbers.get(minIndex) > numbers.get(j)) {
+                    minIndex = j;
+                }
+            }
+            int temp = numbers.get(minIndex);
+            numbers.set(minIndex, numbers.get(i));
+            numbers.set(i, temp);
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
